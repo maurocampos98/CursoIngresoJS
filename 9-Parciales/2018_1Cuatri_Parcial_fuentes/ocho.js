76fffe4427c1fa162efc5a47d01ea887ed89f250
maurocampos98 <55158808+maurocampos98@users.noreply.github.com>
2020-03-03 18:02:00 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,72 @@
 function mostrar()
 {
+    var letra;
+    var numero;
+    var contador=00;
+    var cantPares=00;
+    var cantImpares=00;
+    var cantCeros=00;
+    var Promedio;
+    var negativos; 
+    var acumPositivos;
+    var max;
+    var min;
+    var respuesta='si';
+    
 
+
+    while (respuesta =='si'){
+        contador++;
+
+        acumPositivos=numero; 
+        negativos=numero;
+        letra=prompt("Ingresar alguna letra de la A a la Z : ");
+
+    while (letra.toUpperCase() < "A" || letra.toUpperCase() > "Z"){
+        letra=prompt("letra no valida, ingrese nuevamente ");
+    }
+
+    numero=prompt("Ingresar algun numero del -100 al 100 : ");
+
+    while (numero <-100 || numero > 100 || isNaN(numero)){
+    numero=prompt("número no valido, ingrese nuevamente: ");  
+    
+      }
+
+      if (numero%2==00){
+        cantPares++;
+        }
+        else {
+            cantImpares++;
+        }
+
+       if (numero==00){
+        cantCeros++;
+       }
+
+       if (numero>=00){
+           acumPositivos=parseInt(acumPositivos);
+           numero=parseInt(numero);
+           acumPositivos=(acumPositivos+numero);
+           Promedio=acumPositivos/contador; 
+        } 
+        else {
+            negativos=parseInt(negativos);
+            numero=parseInt(numero);
+            negativos=(negativos+numero);
+         } 
+
+
+      respuesta=prompt("¿desea continuar? ");
+
+    }
+
+    alert("cantidad de numeros pares:" + cantPares );
+    alert("cantidad de numeros impares: " + cantImpares );
+    alert("cantidad de ceros: " + cantCeros );
+    alert("promedio: " + Promedio );
+    alert("la suma de numeros negativos es : " + negativos );
+   /*  alert("numero maximo: ");
+    alert("letra del numero maximo: "); 
+    */ 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,73 @@
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var sexoNotaBaja;
+    var promedio;
+    var acumNota=0;
+    var acumVaron=0;
+    var min=0;
+    var max=0;
+    
+    //REPETIR 5 VECES (SE PIDEN LAS NOTAS DE 5 ALUMNOS).
 
+    for(var i=0 ; i<3 ; i++){
+     
+      
+    // PERMITIR EL INGRESO POR PROMPT DE LAS NOTAS.  
+
+    nota=prompt("ingresar nota ");
+    nota=parseInt(nota);
+
+    // VALIDAR LAS NOTAS Y EL SEXO.
+
+    while (nota < 0 || nota > 10 || isNaN(nota)){
+        nota=prompt("nota invalida, ingrese otra");
+        nota=parseInt(nota); 
+       
+        }
+
+        sexo=prompt("Ingresar sexo del alumno");
+    while (sexo !='m' && sexo !='f'){
+        sexo=prompt("sexo invalido,ingrese nuevamente");
+        }
+
+    // PROMEDIO DE LAS NOTAS TOTALES.    
+      acumNota=nota+acumNota;
+      acumNota=parseInt(acumNota);   
+      promedio=acumNota/3
+      promedio=parseInt(promedio); 
+
+    // BUSCAR LA NOTA MINIMA Y MAXIMA // Y MOSTRAR EL SEXO DE LA PERSONA CON NOTA MAS BAJA. 
+
+      if (i==0){
+        max=nota;
+        min=nota;
+        sexoNotaBaja=sexo;
+       }
+         else if (nota > max){
+            max=nota;    
+         }
+           if (nota < min){
+              min=nota;
+              sexoNotaBaja=sexo;
+         }
+
+    // ACUMULAR LA CANTIDAD DE VARONES QUE SACARON MAS DE 6.
+
+       if (sexo =='m' && nota >= 6) {
+         acumVaron++ 
+       }
+
+      }    
+
+    // MOSTRAR EN PANTALLA LOS ITEMS PEDIDOS. 
+
+      alert("el promedio de todas las notas es de " + promedio + 
+      " la nota mas alta es de " + max + " y la mas baja es de " + min + 
+      " el sexo de la persona con nota mas baja es " + sexoNotaBaja
+      + " y la cantidad de varones que sacaron 6 o más es de " + acumVaron );
 }
+
+
+
